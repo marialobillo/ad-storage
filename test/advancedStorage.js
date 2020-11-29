22ef1fe04dce96a4532b2e043360d948cfcc2b6a
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-11-29 20:49:45 +0100

    refactoring the tests

--- test/advancedStorage.js
+++ test/advancedStorage.js
@@ -2,16 +2,20 @@ const AdvancedStorage = artifacts.require('AdvancedStorage');
 
 contract('AdvancedStorage', () => {
 
-    it('Should add an element to ids array', async () => {
-        const advancedStorage = await AdvancedStorage.deployed();
-        await advancedStorage.add(10);
+    let advancedStorage = null;
 
+    beforeEach(async () => {
+        advancedStorage = await AdvancedStorage.deployed();
+    });
+
+    it('Should add an element to ids array', async () => {
+        
+        await advancedStorage.add(10);
         const result = await advancedStorage.ids(0);
         assert(result.toNumber() === 10);
     });
 
     it('Should get an element from ids array', async () => {
-        const advancedStorage = await AdvancedStorage.deployed();
         await advancedStorage.add(20);
 
         const result = await advancedStorage.get(1);
@@ -19,7 +23,6 @@ contract('AdvancedStorage', () => {
     });
 
     it('Should gets the ids array', async () => {
-        const advancedStorage = await AdvancedStorage.deployed();
         const rawIds = await advancedStorage.getAll();
         
         const ids = rawIds.map(id => id.toNumber());
@@ -27,7 +30,6 @@ contract('AdvancedStorage', () => {
     });
 
     it('Should gets the length of the ids array', async () => {
-        const advancedStorage = await AdvancedStorage.deployed();
         const length = await advancedStorage.length();
         
         assert(length.toNumber() === 2);
